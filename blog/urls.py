341f49b8ec8bd4d3c513aff18c2db5d0d30ec3a3
justErkinov <uzcoding54@gmail.com>
2025-05-15 17:48:02 +0500

    Create all files and add

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('post/<int:pk>/delete', views.BlogDeleteView.as_view(), name = 'post_delete'),
+
+    path('post/<int:pk>/edit', views.BlogUpdateView.as_view(), name = 'post_edit'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'post_new'),
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
